aa4ff3837d4d68f8af314847cdcc968531330840
Roy <roy.adan.trenal@gmail.com>
2025-10-24 12:07:43 -0400

    feat: clean app component and create product page

---
+++ src/pages/product.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product: React.FC = () => {
+  return (
+    <div>
+      <h1>Página Principal de Productos</h1>
+    </div>
+  );
+};
+
+export default Product;
