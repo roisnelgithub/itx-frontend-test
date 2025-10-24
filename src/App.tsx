27dde783fa6460454434b138e777003757406def
roisnelgithub <147262667+roisnelgithub@users.noreply.github.com>
2025-10-24 13:50:40 -0400

    Feature/Configuración inicial (#2)
    * Feature/Iniciar proyecto (#1)
    
    * feat(init): initialize project with Vite, React, and TypeScript"
    
    * chore(gitignore): ignore Vite cache folder .vite
    
    * chore: install react-router-dom
    
    * feat: clean app component and create product page
    
    * chore: install Tailwind CSS
    
    * chore: install Shadcn
    
    * chore: install Axios and TanStack Query
    
    * chore: setup Vitest with basic test
    
    * chore: clean up app, leaving only the title and organize imports

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Product from './pages/product'
+import './index.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/product.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import Product from './product';
+
+describe('Product Page', () => {
+  it('renders title correctly', () => {
+    render(<Product />);
+    expect(screen.getByText(/Página Principal de Productos/i)).toBeInTheDocument();
+  });
+});
