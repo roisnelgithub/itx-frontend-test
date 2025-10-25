8f12e66fcb70a996a00f82aec5acf507adcc672e
Roy <roy.adan.trenal@gmail.com>
2025-10-25 11:34:29 -0400

    chore: add Shadcn UI tooltip

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
+import { TooltipProvider } from './components/ui/tooltip';
 
 import App from './App';
 import './index.css';
@@ -9,7 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
