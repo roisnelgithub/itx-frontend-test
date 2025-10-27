665235c2c9f1c9900cb73017e3736dc23614832e
Roy <roy.adan.trenal@gmail.com>
2025-10-27 13:47:35 -0400

    fix: basic render test for ProductPage component

--- src/pages/product.page.test.tsx
+++ src/pages/product.page.test.tsx
@@ -2,10 +2,17 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
 import ProductPage from './product.page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 describe('Product Page', () => {
   it('renders title correctly', () => {
-    render(<ProductPage />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ProductPage />
+      </QueryClientProvider>
+    );
     expect(screen.getByText(/Product list/i)).toBeInTheDocument();
   });
 });
